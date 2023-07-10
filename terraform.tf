1d5cf43c2ead449d031875d1e32899f17b7f3b31
“Usun” <dasnubz@outlook.com>
2023-07-10 02:14:19 +0100

    4 commits

--- terraform.tf
+++ terraform.tf
@@ -1,22 +1,20 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-terraform {
-
-  /* Uncomment this block to use Terraform Cloud for this tutorial
+terraform { 
   cloud {
-      organization = "organization-name"
+      organization = "snubz_aws_learn"
       workspaces {
         name = "learn-terraform-variables"
       }
   }
-  */
+ 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.15.0"
+      version = "~> 4.4.0"
     }
   }
-  required_version = "~> 1.2.0"
+  required_version = "~> 1.2"
 }
 
